90d4b0f3c9ed12b82bc30babeb81cb05993857b7
seannyandusi <seanmotanya@gmail.com>
2023-10-09 15:51:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for(let i = 0; i < array.length; i++) {
+    for(let j = i + 1; j < array.length; j++) {
+        if(array[i] + array[j] === target) {
+            return true;
+        }
+    }
+}
+return false;
+
+
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n^2)
 */
 
 /* 
@@ -12,6 +23,13 @@ function hasTargetSum(array, target) {
 
 /*
   Add written explanation of your solution here
+  This function, checkSumTarget, takes two parameters: an array of numbers (array) and a target number (target).
+
+The function works by iterating over each element in the array with a pair of nested loops. The outer loop (for(let i = 0; i < array.length; i++)) iterates over each element in the array, and the inner loop (for(let j = i + 1; j < array.length; j++)) iterates over each subsequent element. This ensures that every unique pair of elements is considered.
+
+Inside the inner loop, we check if the sum of the current pair of numbers equals the target (if(array[i] + array[j] === target)). If it does, we immediately return true and exit the function. This means that as soon as we find a pair of numbers that add up to the target, we stop checking any further.
+
+If we finish checking all pairs of numbers and haven’t returned true, that means no pair of numbers adds up to the target. In this case, we return false
 */
 
 // You can run `node index.js` to view these console logs
